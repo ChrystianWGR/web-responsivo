de17c1ca061b4d3afe12fe20a14933ed43c01927
Miguel Maletzke <88055057+MiguelMA3@users.noreply.github.com>
2024-06-27 14:38:19 +0000

    Correcoes ortograficas e ajustes

--- js/aplicativo.js
+++ js/aplicativo.js
@@ -5,13 +5,6 @@ function getDistanceFromTheTop(element) {
   return document.querySelector(id).offsetTop;
 }
 
-// function nativeScroll(distanceFromTheTop) {
-//   window.scroll({
-//     top: distanceFromTheTop,
-//     behavior: "smooth",
-//   });
-// }
-
 function scrollToSection(event) {
   event.preventDefault();
   const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
@@ -22,14 +15,12 @@ menuLinks.forEach((link) => {
   link.addEventListener("click", scrollToSection);
 });
 
-function smoothScrollTo(endX, endY, duration) {
+function smoothScrollTo(endX, endY, duration = 900) {
   const startX = window.scrollX || window.pageXOffset;
   const startY = window.scrollY || window.pageYOffset;
   const distanceX = endX - startX;
   const distanceY = endY - startY;
   const startTime = new Date().getTime();
-
-  duration = typeof duration !== "undefined" ? duration : 900;
 
   const easeInOutQuart = (time, from, distance, duration) => {
     if ((time /= duration / 2) < 1)
@@ -37,13 +28,18 @@ function smoothScrollTo(endX, endY, duration) {
     return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
   };
 
-  const timer = setInterval(() => {
-    const time = new Date().getTime() - startTime;
-    const newX = easeInOutQuart(time, startX, distanceX, duration);
-    const newY = easeInOutQuart(time, startY, distanceY, duration);
-    if (time >= duration) {
-      clearInterval(timer);
+  function scroll() {
+    const currentTime = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(currentTime, startX, distanceX, duration);
+    const newY = easeInOutQuart(currentTime, startY, distanceY, duration);
+
+    if (currentTime < duration) {
+      window.scrollTo(newX, newY);
+      requestAnimationFrame(scroll);
+    } else {
+      window.scrollTo(endX, endY);
     }
-    window.scroll(newX, newY);
-  }, 1000 / 60);
+  }
+
+  requestAnimationFrame(scroll);
 }
